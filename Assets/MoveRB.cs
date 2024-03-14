8519f8f94b392ee70325be56c5c3543319505ced
BankPry <parunyoo.nijj@bumail.net>
2024-03-15 06:38:48 +0700

    Initial Project Commit

---
+++ Assets/MoveRB.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRB : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontal, 0f, vertical) * moveSpeed;
+        rb.AddForce(movement);
+    }
+}
